917055ff3c2e8960aa2808132f206876c10a868c
redyarr <104366409+redyarr@users.noreply.github.com>
2022-10-29 02:40:29 +0300

    Add files via upload

---
+++ javaScript WEBSITE/first/first.js
@@ -0,0 +1,21 @@
+console.log("hello");
+console.log("its really nice to meet you!");
+
+//window.alert("i realy like you :)")
+
+// let is a variable that can store String or integer or boolean
+
+
+var goal= 10;
+let r= "redyar";
+
+let c = true;
+
+for(let i = 0;i<goal; i++ ){
+
+    console.log(i);
+
+}
+
+document.getElementById("p1").innerHTML = "hello " + r;
+
